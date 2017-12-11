74360a1b45a08ac58b054ccfb7ca09aae5cf002e
Jenya Korolenko <jeyakoroll@gmail.com>
2017-12-11 22:51:57 +0200

    form static is done

--- source/js/main.js
+++ source/js/main.js
@@ -3,32 +3,37 @@ $(document).ready(function() {
 
 	// about gallery
 	topGallery.owlCarousel({
-		// animateOut: 'slideOutDown',
-  //   animateIn: 'flipInX',
+		animateOut: 'slideOutDown',
+    animateIn: 'flipInX',
+    autoplay: true,
+    autoplayTimeout: 4000,
     responsiveClass:true,
+    center: true,
+    autoWidth: true,
+    loop:true,
+    items: 1,
+    nav: false,
+    dots: true,
     responsive:{
         0:{
-				    loop:true,
-				    margin: 150,
-				    items: 1,
-				    center: true,
-				    dots: true,
-				    nav: false,
-				    autoplay: true,
-				    autoWidth: true
+				    margin: 150
         },
         992:{
-            loop:true,
-				    margin: 20,
-				    items: 1,
-				    center: true,
-				    dots: true,
-				    nav: false,
-				    autoplay: true,
-				    autoWidth: true
+				    margin: 20
         }
     }
   });
+
+  // style for form
+			$( '.form__wrap input[name=name_last]' ).closest( '.form-group' ).after(
+        '<div class="form-group"><select class="form-control footer__form-select" required name="size"><option selected="selected" value="" class="option" style="background-color: transparent;">Выберите размер</option><option value="xs" class="option" style="background-color: transparent;">40</option><option value="x" class="option" style="background-color: transparent;">41</option><option value="red" class="option" style="background-color: transparent;">42</option><option value="grey" class="option" style="background-color: transparent;">43</option><option value="grey" class="option" style="background-color: transparent;">44</option><option value="grey" class="option" style="background-color: transparent;">45</option></select></div>'
+    );
+
+    $( '.form__wrap .order-form' ).addClass( 'footer__form-order' );
+    $( '.form__wrap input' ).addClass( 'footer__form-data' );
+    $( '.form__wrap button' ).addClass( 'footer__form-button' );
+    $('.form__wrap input[name=name_first]').attr("placeholder","Имя");
+    $('.form__wrap input[name=name_last]').attr("placeholder","Телефон");
 	
 
 
